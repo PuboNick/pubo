0f5d47caa13ca1e17d5aa8c663298e7c362171dc
Jiang Jinming <360104322@qq.com>
2022-06-01 16:48:54 +0800

    feat(pubo-web):add websocket

--- packages/pubo-web/src/index.ts
+++ packages/pubo-web/src/index.ts
@@ -4,5 +4,16 @@ import { downloadFile } from './file/download';
 import { WebStorage } from './storage';
 import { createConfigureRos } from './ros';
 import { RosConfigure } from './ros/factory/configure';
+import WebsocketClient from './websocket/client';
 
-export { loadScript, blob2text, blob2base64, blob2file, downloadFile, WebStorage, createConfigureRos, RosConfigure };
+export {
+  loadScript,
+  blob2text,
+  blob2base64,
+  blob2file,
+  downloadFile,
+  WebStorage,
+  createConfigureRos,
+  RosConfigure,
+  WebsocketClient,
+};
