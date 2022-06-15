a1bd2ef1a9dcb126b7b4d472cb1a7af708b1380b
Jiang Jinming <360104322@qq.com>
2022-06-15 10:12:47 +0800

    feat(debounce):add run first or last option

--- packages/pubo-utils/src/debounce/index.ts
+++ packages/pubo-utils/src/debounce/index.ts
@@ -1,14 +1,24 @@
-export const debounce = (cb: any, time: number) => {
-  let shouldRun = true;
-  let t;
-  return (...args) => {
-    if (shouldRun) {
-      cb(...args);
-      shouldRun = false;
-    }
-    if (t) {
-      clearTimeout(t);
-    }
-    t = setTimeout(() => (shouldRun = true), time);
-  };
+export const debounce = (cb: any, time: number, first = true) => {
+  if (first) {
+    let shouldRun = true;
+    let t;
+    return (...args) => {
+      if (shouldRun) {
+        cb(...args);
+        shouldRun = false;
+      }
+      if (t) {
+        clearTimeout(t);
+      }
+      t = setTimeout(() => (shouldRun = true), time);
+    };
+  } else {
+    let t;
+    return (...args) => {
+      if (t) {
+        clearTimeout(t);
+      }
+      t = setTimeout(() => cb(...args), time);
+    };
+  }
 };
