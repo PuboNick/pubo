ac2a3d5151bb6cefc6af68f0f85a954e93212529
Jiang Jinming <360104322@qq.com>
2022-09-22 14:41:35 +0800

    feat(watch-dog):添加看门狗

---
+++ packages/pubo-utils/src/watch-dog/index.ts
@@ -0,0 +1,24 @@
+interface WatchDogProps {
+  limit: number;
+  onTimeout: () => void;
+}
+
+export class WatchDog {
+  private readonly limit: number = 10;
+  private readonly onTimeout: () => void;
+  private timeout: any = null;
+
+  constructor({ limit = 10, onTimeout }: WatchDogProps) {
+    this.limit = limit;
+    this.onTimeout = onTimeout;
+  }
+
+  feed() {
+    clearTimeout(this.timeout);
+    this.init();
+  }
+
+  init() {
+    this.timeout = setTimeout(() => this.onTimeout(), this.limit * 1000);
+  }
+}
