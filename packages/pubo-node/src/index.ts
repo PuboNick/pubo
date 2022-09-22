a159e257e0d79f0ddd171bed197718e17155d483
Jiang Jinming <360104322@qq.com>
2022-09-22 14:57:19 +0800

    feat()

--- packages/pubo-node/src/index.ts
+++ packages/pubo-node/src/index.ts
@@ -1,4 +1,5 @@
 export { JsonStorage } from './storage/json';
+export { initRtspVideos } from './rtsp-video';
 export { FtpClient } from './ftp-client';
 export { md5 } from './md5';
 export { createGrpcService } from './grpc';
