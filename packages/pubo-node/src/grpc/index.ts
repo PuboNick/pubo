b1dd1d76e9f72a7faacfc1d687e9fbe4cf785900
Jiang Jinming <360104322@qq.com>
2022-09-23 11:49:34 +0800

    feat(grpc):修改grpc客户端

--- packages/pubo-node/src/grpc/index.ts
+++ packages/pubo-node/src/grpc/index.ts
@@ -1,13 +1,29 @@
-import { superFactory } from 'pubo-utils';
-
-import { GrpcService } from './GrpcService';
-
-interface GrpcServiceOptions {
-  client: string;
-  requestType: string;
-  service?: string;
+interface CreateClientProps {
+  url: string;
+  options?: any;
+  ServiceImp: any;
+  Grpc: any;
 }
 
-export const createGrpcService = superFactory<GrpcServiceOptions, GrpcService>((config, key) => {
-  return new GrpcService({ ...config, serviceName: config.service || key });
-});
+type RpcImpl = (service: string, method: string, data: Uint8Array) => Promise<Uint8Array>;
+
+export function createRpcClient<T>({ url, options = {}, ServiceImp, Grpc }: CreateClientProps): T {
+  const opt = { 'grpc.max_send_message_length': -1, 'grpc.max_receive_message_length': -1, ...options };
+  const connection = new Grpc.Client(url, Grpc.credentials.createInsecure(), opt);
+
+  const request: RpcImpl = (service, method, data) => {
+    const path = `/${service}/${method}`;
+    return new Promise((resolve, reject) => {
+      const resultCallback: any = (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      };
+      const passThrough = (argument: any) => argument;
+      connection.makeUnaryRequest(path, passThrough, passThrough, data, resultCallback);
+    });
+  };
+  return new ServiceImp({ request });
+}
