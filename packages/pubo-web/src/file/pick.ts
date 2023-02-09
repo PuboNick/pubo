40c369f5ec5f47a534a01e7f02c720159477758a
Jiang Jinming <360104322@qq.com>
2023-02-09 14:02:10 +0800

    feat(pubo-web):优化文件选择

--- packages/pubo-web/src/file/pick.ts
+++ packages/pubo-web/src/file/pick.ts
@@ -1,15 +1,27 @@
-export const pickFiles = (): Promise<File[]> => {
+export const pickFiles = (): Promise<File[] | null> => {
   return new Promise((resolve) => {
     const el = document.createElement('input');
+    const destroy = () => {
+      document.body.removeChild(el);
+    };
+    const onFocus = () => {
+      resolve(null);
+      destroy();
+      window.removeEventListener('focus', onFocus);
+    };
     el.type = 'file';
     el.style.visibility = 'hidden';
     el.style.position = 'fixed';
     el.style.top = '0px';
     el.style.zIndex = '-1';
+    window.addEventListener('focus', onFocus);
+
     el.onchange = (e: any) => {
       resolve(e.target.files);
-      document.body.removeChild(el);
+      window.removeEventListener('focus', onFocus);
+      destroy();
     };
+
     document.body.appendChild(el);
     el.click();
   });
