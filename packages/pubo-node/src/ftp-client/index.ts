2ad856733096675b841b78d229c0f890dcfc6f4b
Jiang Jinming <360104322@qq.com>
2023-03-31 09:25:06 +0800

    feat(pubo-node):优化ftp客户端，增加连接池

--- packages/pubo-node/src/ftp-client/index.ts
+++ packages/pubo-node/src/ftp-client/index.ts
@@ -1,7 +1,7 @@
-import { SyncQueue } from 'pubo-utils';
+import { random, waitFor } from 'pubo-utils';
 import type { Stream } from 'stream';
 
-interface FtpFile {
+export interface FtpFile {
   name: string;
   owner: string;
   group: string;
@@ -10,74 +10,177 @@ interface FtpFile {
   type: string;
 }
 
-interface FtpConnectOptions {
+export interface FtpConnectOptions {
   user: string;
   host: string;
   password: string;
+  driver: any;
 }
 
-type GetFile = (path: string) => Promise<Stream>;
+type GetFile = (path: string) => Promise<Buffer>;
 type PutFile = (input: string | Buffer | Stream, path: string) => Promise<string>;
 type DeleteFile = (path: string) => Promise<string>;
 type ListFiles = (path: string) => Promise<FtpFile[]>;
 type RenameFile = (path: string, old: string) => Promise<string>;
 
-export class FtpClient {
+export interface FtpClientInterface {
+  get: GetFile;
+  put: PutFile;
+  delete: DeleteFile;
+  list: ListFiles;
+  rename: RenameFile;
+}
+
+export class FtpClient implements FtpClientInterface {
   private readonly driver: any;
   private readonly options: any;
-  private readonly queue = new SyncQueue();
 
-  constructor({ driver, ...options }: { driver: any } & FtpConnectOptions) {
+  private readonly state = { running: false, connected: false, destroyed: false, connecting: false };
+  private client: any;
+  private _len = 0;
+
+  id = random();
+
+  constructor({ driver, ...options }: FtpConnectOptions) {
     this.driver = driver;
     this.options = options;
   }
 
-  get: GetFile = this._bind('get');
-  put: PutFile = this._bind('put');
-  delete: DeleteFile = this._bind('delete');
-  list: ListFiles = this._bind('list');
-  rename: RenameFile = this._bind('rename');
-
-  private _push(cb): any {
-    return (...args) => this.queue.push(() => cb.call(this, ...args));
+  get len() {
+    return this._len;
   }
 
-  private _bind(fn: string) {
-    return this._push(async (...args) => {
-      const c = await this.connect();
-      return this._run(fn, c, args);
+  set len(n) {
+    this._len = n;
+    if (this._len < 1) {
+      this.close();
+    }
+  }
+
+  put = this.bind('put');
+  delete = this.bind('delete');
+  list = this.bind('list');
+  rename = this.bind('rename');
+
+  private async connect(): Promise<any> {
+    if (!this.client) {
+      this.client = new this.driver();
+    }
+    this.state.destroyed = false;
+    if (this.state.connecting) {
+      await waitFor(() => this.state.connected, { checkTime: 1000, timeout: 10000 });
+      return 'connected';
+    }
+    this.state.connecting = true;
+    return new Promise((resolve, reject) => {
+      this.client.once('ready', () => {
+        this.state.connected = true;
+        resolve('connected');
+        this.state.connecting = false;
+      });
+      this.client.once('error', (err) => {
+        reject(err);
+        this.close();
+      });
+      this.client.connect({ ...this.options });
     });
   }
 
-  private _run(fn, c, args) {
+  private close() {
+    this.client.end();
+    this.state.connected = false;
+    this.state.destroyed = true;
+    this.state.connecting = false;
+    this.client = null;
+  }
+
+  private async run({ fn, args }: { fn: string; args: any[] }): Promise<any> {
+    this.len += 1;
+    if (!this.state.connected) {
+      await this.connect();
+    }
+    if (this.state.running) {
+      await waitFor(() => !this.state.running, { checkTime: 1000, timeout: 6000000 });
+    }
+    this.state.running = true;
+
     return new Promise((resolve, reject) => {
-      c[fn](...args, (err, res) => {
+      this.client[fn](...args, (err, res) => {
         if (err) {
           reject(err);
-          c.end();
         } else {
-          resolve(res || 'operation successful.');
-          if (fn === 'get') {
-            res.once('close', () => c.end());
-          } else {
-            c.end();
-          }
+          resolve(res);
         }
       });
     });
   }
 
-  private connect(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      const c: any = new this.driver();
-      c.once('ready', () => {
-        resolve(c);
+  private bind(fn: string) {
+    return async (...args): Promise<any> => {
+      const res = await this.run({ fn, args });
+      this.state.running = false;
+      this.len -= 1;
+      return res;
+    };
+  }
+
+  async get(...args): Promise<Buffer> {
+    let res = Buffer.alloc(0);
+    const stream = await this.run({ fn: 'get', args });
+
+    return new Promise((resolve) => {
+      stream.on('data', (chunk) => {
+        res = Buffer.concat([res, chunk], res.byteLength + chunk.byteLength);
       });
-      c.once('error', (err) => {
-        reject(err);
-        c.end();
+      stream.on('end', () => {
+        resolve(res);
+        this.state.running = false;
+        this.len -= 1;
       });
-      c.connect({ ...this.options });
     });
+  }
+}
+
+export class FtpClientPool implements FtpClientInterface {
+  private readonly options: FtpConnectOptions;
+  private readonly maxConnection: number;
+  private readonly pool: FtpClient[] = [];
+
+  constructor({ maxConnection = 5, ...options }: { maxConnection?: number } & FtpConnectOptions) {
+    this.options = options;
+    this.maxConnection = maxConnection;
+  }
+
+  get = this.bind('get');
+  put = this.bind('put');
+  delete = this.bind('delete');
+  list = this.bind('list');
+  rename = this.bind('rename');
+
+  get len() {
+    return this.pool.length;
+  }
+
+  private get client() {
+    if (this.pool.length < this.maxConnection) {
+      const client = new FtpClient(this.options);
+      this.pool.push(client);
+      return client;
+    }
+
+    this.pool.sort((a, b) => a.len - b.len);
+    return this.pool[0];
+  }
+
+  private bind(fn) {
+    return async (...args) => {
+      const client = this.client;
+      const res = await client[fn](...args);
+      if (client.len < 1) {
+        const index = this.pool.findIndex((item) => item.id === client.id);
+        this.pool.splice(index, 1);
+      }
+      return res;
+    };
   }
 }
--- packages/pubo-node/src/index.ts
+++ packages/pubo-node/src/index.ts
@@ -1,6 +1,6 @@
 export { JsonStorage } from './storage/json';
 export { initRtspVideos, RtspVideosManager } from './rtsp-video';
-export { FtpClient } from './ftp-client';
+export { FtpClient, FtpClientPool, FtpConnectOptions, FtpFile } from './ftp-client';
 export { md5 } from './md5';
 export { createRpcClient } from './grpc';
 export { PuboFileSystem } from './file-system';
