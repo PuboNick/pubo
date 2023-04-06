70760fbf2e333169838bbccd099b86012adcadee
Jiang Jinming <360104322@qq.com>
2023-04-06 09:09:49 +0800

    feat(rpc):增加ssl证书支持

--- packages/pubo-node/src/grpc/index.ts
+++ packages/pubo-node/src/grpc/index.ts
@@ -3,13 +3,15 @@ interface CreateClientProps {
   options?: any;
   ServiceImp: any;
   Grpc: any;
+  cert?: Buffer;
 }
 
 type RpcImpl = (service: string, method: string, data: Uint8Array) => Promise<Uint8Array>;
 
-export function createRpcClient<T>({ url, options = {}, ServiceImp, Grpc }: CreateClientProps): T {
+export function createRpcClient<T>({ url, options = {}, ServiceImp, Grpc, cert }: CreateClientProps): T {
   const opt = { 'grpc.max_send_message_length': -1, 'grpc.max_receive_message_length': -1, ...options };
-  const connection = new Grpc.Client(url, Grpc.credentials.createInsecure(), opt);
+  const credentials = cert ? Grpc.credentials.createSsl(cert) : Grpc.credentials.createInsecure();
+  const connection = new Grpc.Client(url, credentials, opt);
 
   const request: RpcImpl = (service, method, data) => {
     const path = `/${service}/${method}`;
