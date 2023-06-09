9d52c7717d7d58a23d91de645bacd2961b9032d6
Jiang Jinming <360104322@qq.com>
2023-06-09 10:46:11 +0800

    feat(style):update code style

--- packages/pubo-node/src/child-process/index.ts
+++ packages/pubo-node/src/child-process/index.ts
@@ -3,5 +3,6 @@ import { ChildProcess } from 'child_process';
 export const createChildProcess = (fn, ...args): ChildProcess => {
   const subprocess = fn(...args);
   process.on('SIGTERM', () => subprocess.kill());
+
   return subprocess;
 };
