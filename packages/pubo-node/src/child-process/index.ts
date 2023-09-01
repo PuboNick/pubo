f6208447ab2bd11998a46ce262bea2de2064a852
Jiang Jinming <360104322@qq.com>
2023-09-01 18:20:19 +0800

    feat(kill):优化杀死进程方法

--- packages/pubo-node/src/child-process/index.ts
+++ packages/pubo-node/src/child-process/index.ts
@@ -1,15 +1,5 @@
-import { ChildProcess } from 'child_process';
 import kill from 'tree-kill';
 
-export const createChildProcess = (fn, ...args): ChildProcess => {
-  const subprocess = fn(...args);
-  const dispose = subprocess.kill.bind(subprocess);
-  subprocess.kill = () => {
-    dispose();
-    kill(parseInt(subprocess.pid));
-  };
-  process.on('SIGTERM', () => subprocess.kill());
-  process.on('exit', () => subprocess.kill());
-
-  return subprocess;
+export const SIGKILL = (pid: number) => {
+  kill(pid, 'SIGKILL');
 };
--- packages/pubo-node/src/index.ts
+++ packages/pubo-node/src/index.ts
@@ -2,6 +2,6 @@ export { JsonStorage } from './storage/json';
 export { FtpClient, FtpClientPool, FtpConnectOptions, FtpFile } from './ftp-client';
 export { createRpcClient } from './grpc';
 export { PuboFileSystem } from './file-system';
-export { createChildProcess } from './child-process';
+export { SIGKILL } from './child-process';
 export { isPortAvailable } from './utils';
 export { pitch } from './pitch';
