d393fa2cff0364221b1879543796b4531945e8d4
Jiang Jinming <360104322@qq.com>
2023-11-03 15:56:24 +0800

    feat(pubo-node):增加grpc 超时

--- packages/pubo-node/src/grpc/index.ts
+++ packages/pubo-node/src/grpc/index.ts
@@ -6,28 +6,71 @@ interface CreateClientProps {
   cert?: Buffer;
 }
 
-type RpcImpl = (service: string, method: string, data: Uint8Array) => Promise<Uint8Array>;
+class GrpcClient {
+  private readonly url: string;
+  private readonly options: any;
+  private readonly Grpc: any;
+  private readonly credentials: any;
+  private client: any;
 
-export function createRpcClient<T>({ url, options = {}, ServiceImp, Grpc, cert }: CreateClientProps): T {
-  const opt = { 'grpc.max_send_message_length': -1, 'grpc.max_receive_message_length': -1, ...options };
-  const credentials = cert ? Grpc.credentials.createSsl(cert) : Grpc.credentials.createInsecure();
-  let connection = new Grpc.Client(url, credentials, opt);
+  constructor({ url, options = {}, Grpc, cert }: any) {
+    this.url = url;
+    this.Grpc = Grpc;
+    const opt = { 'grpc.max_send_message_length': -1, 'grpc.max_receive_message_length': -1, ...options };
+    const credentials = cert ? Grpc.credentials.createSsl(cert) : Grpc.credentials.createInsecure();
+    this.credentials = credentials;
+    this.options = opt;
+    this.options.timeout = this.options.timeout ?? 5000;
+    this.client = new Grpc.Client(url, credentials, opt);
+  }
 
-  const request: RpcImpl = (service, method, data) => {
+  reset() {
+    if (this.client) {
+      this.client.close();
+      delete this.client;
+    }
+
+    this.client = new this.Grpc.Client(this.url, this.credentials, this.options);
+  }
+
+  passThrough(argument) {
+    return argument;
+  }
+
+  request(service, method, data) {
     const path = `/${service}/${method}`;
+    let timeout: any;
+    let ended = false;
     return new Promise((resolve, reject) => {
-      const resultCallback: any = (err, res) => {
+      const request = this.client.makeUnaryRequest(path, this.passThrough, this.passThrough, data, (err, res) => {
+        if (ended) {
+          return;
+        }
+        clearTimeout(timeout);
+
         if (err) {
-          connection.close();
-          connection = new Grpc.Client(url, credentials, opt);
+          this.reset();
           reject(err);
         } else {
           resolve(res);
         }
-      };
-      const passThrough = (argument: any) => argument;
-      connection.makeUnaryRequest(path, passThrough, passThrough, data, resultCallback);
+      });
+
+      if (this.options.timeout < 0) {
+        return;
+      }
+
+      timeout = setTimeout(() => {
+        ended = true;
+        request.cancel();
+        this.reset();
+        reject(new Error('timeout'));
+      }, this.options.timeout);
     });
-  };
-  return new ServiceImp({ request });
+  }
+}
+
+export function createRpcClient<T>({ url, options = {}, ServiceImp, Grpc, cert }: CreateClientProps): T {
+  const client = new GrpcClient({ url, options, Grpc, cert });
+  return new ServiceImp({ request: client.request.bind(client) });
 }
