06579b6f66118e155b2ca71ed4aaada83532f543
pubo <360104322@qq.com>
2024-04-26 11:07:51 +0800

    feat(network):增加获取wifi网卡名称函数

---
+++ packages/pubo-node/src/utils/network.ts
@@ -0,0 +1,38 @@
+import { exec } from 'child_process';
+
+function parseNetworkData(data) {
+  return data
+    .toString()
+    .replace(/\*-network:.*\n/g, '-----')
+    .split('-----')
+    .filter((item) => item.trim())
+    .map((item) => {
+      const res: any = {};
+      const tmp = item.split('\n');
+      tmp.forEach((i) => {
+        const arr = i.split(':').map((key) => key.trim());
+        if (arr[0] && arr[1]) {
+          res[arr[0]] = arr[1];
+        }
+      });
+      return res;
+    });
+}
+
+export async function getNetworks(): Promise<any[]> {
+  return new Promise((resolve) => {
+    const child = exec('lshw -C network');
+    child.stdout?.on('data', (data) => {
+      resolve(parseNetworkData(data));
+    });
+  });
+}
+
+export async function getWifiName() {
+  const networks = await getNetworks();
+  const wifi = networks.find((item) => item.description === 'Wireless interface');
+  if (!wifi) {
+    return '';
+  }
+  return wifi['logical name'];
+}
