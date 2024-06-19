0abc38d8bbab27caed4c423a1dd53da8ebfe8b34
pubo <360104322@qq.com>
2024-06-19 13:45:11 +0800

    feat(build):619

--- packages/pubo-node/src/child-process/index.ts
+++ packages/pubo-node/src/child-process/index.ts
@@ -18,22 +18,24 @@ export async function getProcessName(pid): Promise<string> {
   });
 }
 
-export async function isProcessAlive(pid) {
+export async function isProcessDied(pid) {
   const name = await getProcessName(pid);
   return name.split(':')[2]?.trim() === 'No such file or directory';
 }
 
 export async function SIGKILL(pid: number, type = 2) {
+  const signal = type === 9 ? 'SIGKILL' : 'SIGINT';
+  require('tree-kill')(pid, signal);
+
   if (process.platform === 'win32') {
-    const signal = type === 9 ? 'SIGKILL' : 'SIGINT';
-    require('tree-kill')(pid, signal);
     return;
   }
 
   exec(`kill -${type} ${pid}`);
+  require('tree-kill')(pid, signal);
 
   try {
-    await waitFor(async () => isProcessAlive(pid), { checkTime: 100, timeout: 10000 });
+    await waitFor(async () => isProcessDied(pid), { checkTime: 100, timeout: 10000 });
   } catch (err) {
     await SIGKILL(pid, 9);
   }
--- packages/pubo-node/src/index.ts
+++ packages/pubo-node/src/index.ts
@@ -2,7 +2,7 @@ export { JsonStorage } from './storage/json';
 export { FtpClient, FtpClientPool, FtpConnectOptions, FtpFile } from './ftp-client';
 export { createRpcClient } from './grpc';
 export { PuboFileSystem } from './file-system';
-export { SIGKILL } from './child-process';
+export { SIGKILL, isProcessDied, getProcessName } from './child-process';
 export { isPortAvailable } from './utils';
 export { pitch } from './pitch';
 export { getWifiName, getNetworks } from './utils/network';
