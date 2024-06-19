3a94d63494f2b9519ef1eef47fe1d2ecd715e50e
pubo <360104322@qq.com>
2024-06-19 17:06:57 +0800

    feat: 🎸 pubo-web update pickfiles
    增加取消

--- packages/pubo-web/src/file/pick.ts
+++ packages/pubo-web/src/file/pick.ts
@@ -1,10 +1,14 @@
 export const pickFiles = (): Promise<FileList> => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     const el = document.createElement('input');
+    let picked = false;
 
     const onFocus = () => {
       document.body.removeChild(el);
       window.removeEventListener('focus', onFocus);
+      if (!picked) {
+        reject('no files picked');
+      }
     };
     el.type = 'file';
     el.style.visibility = 'hidden';
@@ -12,7 +16,9 @@ export const pickFiles = (): Promise<FileList> => {
     el.style.top = '0px';
     el.style.zIndex = '-1';
     window.addEventListener('focus', onFocus);
+
     el.onchange = (e: any) => {
+      picked = e.target.files.length > 0;
       resolve(e.target.files);
     };
 
