74128e0a199e8590786691c7060c4c2a0f58f5f7
pubo <360104322@qq.com>
2024-07-30 13:55:07 +0800

    feat: 🎸 pubo-node
    json-storage 增加cluster适配

--- packages/pubo-node/src/storage/json.ts
+++ packages/pubo-node/src/storage/json.ts
@@ -1,28 +1,52 @@
 import { readFileSync, writeFile, mkdirSync } from 'fs';
 import { SyncQueue } from 'pubo-utils';
+import { v4 as uuid } from 'uuid';
 
-export class JsonStorage {
+const cluster = require('cluster');
+
+interface StorageInstance {
+  getState: () => Promise<any>;
+  setState: (state: any) => Promise<any>;
+}
+
+class Manager implements StorageInstance {
   private readonly path: string;
   private _state: any = {};
   private readonly queue = new SyncQueue();
-
-  get state() {
-    return this._state;
-  }
+  private readonly key: string;
 
   constructor(path: string) {
     this.path = path;
+    this.key = encodeURIComponent(path);
+    cluster.on('online', (worker) => {
+      worker.on('message', (message) => this.onMessage(message, worker));
+    });
     this.restore();
   }
 
-  private sync() {
+  private async onMessage(message, worker) {
+    if (message.key !== this.key) {
+      return;
+    }
+
+    let payload;
+    if (message.type === 'get') {
+      payload = await this.getState();
+    } else {
+      payload = await this.setState(message.payload);
+    }
+
+    worker.send({ uid: message.uid, key: this.key, payload });
+  }
+
+  private async sync() {
     if (this.queue.length > 0) {
       return;
     }
-    this.queue.push(this._sync.bind(this));
+    await this.queue.push(this._syncFile.bind(this));
   }
 
-  private async _sync() {
+  private async _syncFile() {
     return new Promise((resolve, reject) => {
       writeFile(this.path, JSON.stringify(this._state), (err) => {
         if (err) {
@@ -39,7 +63,6 @@ export class JsonStorage {
       const buf = readFileSync(this.path);
       this._state = JSON.parse(buf.toString());
     } catch (err) {
-      console.log(err);
       const str = process.platform === 'win32' ? '\\' : '/';
       if (str) {
         mkdirSync(this.path.split(str).slice(0, -1).join(str), { recursive: true });
@@ -48,23 +71,103 @@ export class JsonStorage {
     }
   }
 
-  get(key?: string) {
-    if (!key) {
-      return this.state;
+  async getState() {
+    return this._state;
+  }
+
+  async setState(values) {
+    this._state = values;
+    await this.sync();
+  }
+}
+
+class Worker implements StorageInstance {
+  private readonly key: string;
+  private readonly callback: any = {};
+
+  constructor(path: string) {
+    this.key = encodeURIComponent(path);
+    process.on('message', this.onMessage.bind(this));
+  }
+
+  private onMessage(message) {
+    if (message.key !== this.key) {
+      return;
+    }
+
+    if (typeof this.callback[message.uid] === 'function') {
+      this.callback[message.uid](message.payload);
+      delete this.callback[message.uid];
+    }
+  }
+
+  async call({ type, payload }) {
+    return new Promise((resolve) => {
+      const uid = uuid();
+      this.callback[uid] = (data) => resolve(data);
+      // @ts-ignore
+      process.send({ uid, type, payload, key: this.key });
+    });
+  }
+
+  async getState() {
+    return this.call({ type: 'get', payload: {} });
+  }
+
+  async setState(payload: any) {
+    return this.call({ type: 'set', payload });
+  }
+}
+
+export class JsonStorage {
+  private readonly instance: StorageInstance;
+
+  constructor(path: string, options: { initialState: any } = { initialState: null }) {
+    if (cluster.isPrimary) {
+      this.instance = new Manager(path);
     } else {
-      return this.state[key];
+      this.instance = new Worker(path);
+    }
+
+    if (options?.initialState) {
+      this.merge(options.initialState);
     }
   }
 
-  set(key: string, values: any) {
-    this._state[key] = values;
-    this.sync();
+  private async getState() {
+    return this.instance.getState();
   }
 
-  merge(values: any) {
+  private async setState(state: any) {
+    return this.instance.setState(state);
+  }
+
+  async get(key?: string) {
+    if (!key) {
+      return this.getState();
+    } else {
+      const state = await this.getState();
+      return state[key];
+    }
+  }
+
+  async set(key: string, values: any) {
+    const state = await this.getState();
+    state[key] = values;
+    await this.setState(state);
+  }
+
+  async merge(values: any) {
+    const state = await this.getState();
     for (const key of Object.keys(values)) {
-      this._state[key] = values[key];
+      state[key] = values[key];
     }
-    this.sync();
+    await this.setState(state);
+  }
+
+  async remove(key: string) {
+    const state = await this.getState();
+    delete state[key];
+    await this.setState(state);
   }
 }
