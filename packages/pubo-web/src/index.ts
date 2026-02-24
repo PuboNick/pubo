8346e51b2d36e3821f22b58af8bd42539eda1d8f
Pubo Nick <360104322@qq.com>
2026-02-24 10:24:59 +0800

    feat(pubo-web): update

--- packages/pubo-web/src/index.ts
+++ packages/pubo-web/src/index.ts
@@ -5,5 +5,5 @@ export { WebStorage } from './storage';
 export { WebsocketClient } from './websocket/client';
 export { DragMethod } from './dom/drag';
 export { pickFiles } from './file/pick';
-export { getCookieValue, getCookie, setCookieItem } from './storage/cookie';
 export { IndexedStorage } from './storage/indexed-db/indexed-db-storage';
+export { getCookieValue, getCookie, setCookieItem } from './storage/cookie';
